d58a764073dad01e95437d14a0c8450c12cf3357
Soundhara Pandian <soundharapandian000@gmail.com>
2025-08-01 15:02:59 +0530

    Create  Parallelogram.java

---
+++ Parallelogram.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int star,space,row;
+        for (row = 1; row<= n; row++) 
+        {
+            for (space = 1; space <= n-row; space++) 
+            {
+                System.out.print(" ");
+            }
+            for (star =1;star <=n; star++)
+            {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
