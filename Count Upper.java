d38c081a608ab07cc1fb974d67cb4b975ac50d89
Soundhara Pandian <soundharapandian000@gmail.com>
2025-08-12 15:28:03 +0530

    Create Count Upper.java

---
+++ Count Upper.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int count = 0;
+
+        for (int i = 0; i < word.length(); i++) 
+        {
+            if (Character.isUpperCase(word.charAt(i)))
+            {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
